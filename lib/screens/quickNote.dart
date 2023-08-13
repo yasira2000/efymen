316ba0b80caa46ae8ce1a2af7d741711d1a569ca
thamindumk <thamindu.20@cse.mrt.ac.lk>
2023-08-13 23:37:47 +0530

    add quick note page but not complete

---
+++ lib/screens/quickNote.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuickNote extends StatelessWidget {
+  const QuickNote({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "QuickNote",
+      home: Scaffold(
+        backgroundColor: Colors.amber,
+      ),
+    );
+  }
+}
