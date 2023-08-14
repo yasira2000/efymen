8fe2eeebed07ef2e4a426ca946dd03fd7384c8fe
thamindumk <thamindu.20@cse.mrt.ac.lk>
2023-08-14 19:38:13 +0530

    some changes about app bar

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 
-Widget appBar() {
-  return Container(
-    padding: EdgeInsets.all(10),
-    color: Colors.amber[400],
-    height: 200,
-    width: double.infinity,
-    child: Row(
-      children: [
-        Image.asset("../assets/icons/logo.png"),
-      ],
-    ),
-  );
+class CommonAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CommonAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: AppBar(
+        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Image.asset('assets/icons/logo.png'),
+        ),
+        title: Text(
+          "EFYMEN",
+          style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60);
 }
