0359efba5d57fb6323a8cb325fbea35b3c02aada
thamindumk <thamindu.20@cse.mrt.ac.lk>
2023-08-30 22:08:01 +0530

    description displaying is done

--- lib/screens/dailyRouting.dart
+++ lib/screens/dailyRouting.dart
@@ -24,20 +24,36 @@ class _DailyRoutingState extends State<DailyRouting>
     "SUN"
   ];
   List<TimeTable> timetable = [
-    TimeTable("doing numerical practice questions and train the speech",
-        DateTime(2023, 7, 13, 01, 30), DateTime(2023, 7, 13, 02, 30), 'home'),
+    TimeTable(
+        "doing numerical practice questions and train the speech",
+        DateTime(2023, 7, 13, 01, 30),
+        DateTime(2023, 7, 13, 02, 30),
+        'home',
+        true),
     TimeTable(
         "doing numerical practice questions doing numerical practice questions numerical practice questions",
         DateTime(2023, 7, 13, 11, 30),
         DateTime(2023, 7, 13, 13, 30),
-        'university'),
-    TimeTable("doing numerical practice questions",
-        DateTime(2023, 7, 13, 18, 30), DateTime(2023, 7, 13, 18, 50), 'bodima'),
+        'university',
+        false),
+    TimeTable(
+        "doing numerical practice questions",
+        DateTime(2023, 7, 13, 18, 30),
+        DateTime(2023, 7, 13, 18, 50),
+        'bodima',
+        false),
     TimeTable(
         "doing numerical practice questions doing numerical questions doing aaaa numerical practice questions",
         DateTime(2023, 7, 13, 7, 30),
         DateTime(2023, 7, 13, 09, 30),
-        'home'),
+        'home',
+        true),
+    TimeTable(
+        "doing numerical practice questions",
+        DateTime(2023, 7, 13, 18, 30),
+        DateTime(2023, 7, 13, 18, 50),
+        'bodima',
+        false),
   ];
   int current = 1;
   @override
@@ -149,7 +165,7 @@ class _DailyRoutingState extends State<DailyRouting>
                     Text(
                       DateFormat.MMMM().format(datetime),
                       style: const TextStyle(
-                          fontSize: 10,
+                          fontSize: 12,
                           fontWeight: FontWeight.w600,
                           color: Color(0xFFFFF0C0)),
                     ),
@@ -216,8 +232,10 @@ class _DailyRoutingState extends State<DailyRouting>
                 height: MediaQuery.of(context).size.height - 200,
                 width: MediaQuery.of(context).size.width / 6,
                 child: Stack(
+                  alignment: AlignmentDirectional.center,
                   children: [
-                    Center(
+                    Positioned(
+                      left: 30,
                       child: Container(
                         decoration: BoxDecoration(
                           color: Color(0xFFFFF9E6),
@@ -239,53 +257,154 @@ class _DailyRoutingState extends State<DailyRouting>
                     itemBuilder: (context, index) {
                       return Padding(
                         padding: const EdgeInsets.only(
-                          left: 40,
-                          bottom: 10,
+                          bottom: 15,
                         ),
                         child: Stack(
+                          alignment: AlignmentDirectional.center,
                           children: [
                             Container(
                               width: 250,
                               decoration: BoxDecoration(
                                 borderRadius: BorderRadius.circular(10),
-                                color: Color(0xFFFFEBAA),
+                                color: const Color(0xFFFFEBAA),
                               ),
                               child: Column(
                                 children: [
+                                  const SizedBox(
+                                    height: 12,
+                                  ),
+                                  IntrinsicHeight(
+                                    child: Row(
+                                      children: [
+                                        const SizedBox(
+                                          width: 20,
+                                        ),
+                                        const VerticalDivider(
+                                          color: Color(0xFF707070),
+                                          width: 2,
+                                          thickness: 2,
+                                        ),
+                                        const SizedBox(
+                                          width: 15,
+                                        ),
+                                        TimeWidget(
+                                          time: timetable[index].startingTime,
+                                        ),
+                                        const Text(' - '),
+                                        TimeWidget(
+                                            time: timetable[index].endingTime),
+                                        const SizedBox(
+                                          width: 90,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 38, top: 5, bottom: 5, right: 10),
+                                    child: Text(
+                                      timetable[index].description,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 15),
+                                    ),
+                                  ),
                                   Row(
                                     children: [
                                       const SizedBox(
-                                        width: 5,
-                                      ),
-                                      const VerticalDivider(
                                         width: 10,
+                                      ),
+                                      const Icon(
+                                        Icons.location_on_outlined,
                                         color: Colors.black,
-                                        thickness: 2,
+                                        size: 20,
                                       ),
-                                      TimeWidget(
-                                        time: timetable[index].startingTime,
+                                      const SizedBox(
+                                        width: 8,
                                       ),
-                                      const Text(' - '),
-                                      TimeWidget(
-                                          time: timetable[index].endingTime),
+                                      Text(
+                                        timetable[index].location,
+                                        style: const TextStyle(
+                                            fontSize: 12,
+                                            fontWeight: FontWeight.w500,
+                                            color: Color(0xFFFFC400)),
+                                      )
                                     ],
                                   ),
-                                  Text(timetable[index].description)
+                                  const SizedBox(
+                                    height: 8,
+                                  )
                                 ],
                               ),
                             ),
                             Positioned(
-                              bottom: 10,
-                              left: 20,
+                              left: 30,
                               child: Container(
-                                height: 20,
+                                height: 30,
                                 width: 30,
-                                decoration: BoxDecoration(
+                                decoration: const BoxDecoration(
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black26,
+                                        blurRadius: 2.0,
+                                        spreadRadius: 0.5,
+                                        offset: Offset(0, 1))
+                                  ],
                                   shape: BoxShape.circle,
-                                  color: Colors.red,
+                                  color: Colors.white,
                                 ),
                               ),
                             ),
+                            Positioned(
+                              left: 35,
+                              child: Container(
+                                height: 20,
+                                width: 20,
+                                decoration: const BoxDecoration(
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black26,
+                                        blurRadius: 2.0,
+                                        spreadRadius: -2,
+                                        offset: Offset(1, 1))
+                                  ],
+                                  shape: BoxShape.circle,
+                                  color: Color(0xFFFFD857),
+                                ),
+                              ),
+                            ),
+                            timetable[index].status
+                                ? const Positioned(
+                                    left: 35,
+                                    child: SizedBox(
+                                      height: 20,
+                                      width: 20,
+                                      child: Icon(
+                                        Icons.check,
+                                        color: Colors.white,
+                                        size: 17,
+                                      ),
+                                    ),
+                                  )
+                                : Container(),
+                            Positioned(
+                              right: 53,
+                              top: 7,
+                              child: InkWell(
+                                child: Container(
+                                  height: 20,
+                                  width: 20,
+                                  decoration: const BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      color: Color(0xFFFFC400)),
+                                  child: const Icon(
+                                    Icons.play_arrow,
+                                    color: Colors.white,
+                                    size: 15,
+                                  ),
+                                ),
+                              ),
+                            )
                           ],
                         ),
                       );
@@ -302,20 +421,35 @@ class _DailyRoutingState extends State<DailyRouting>
 class TimeWidget extends StatelessWidget {
   final DateTime time;
   const TimeWidget({super.key, required this.time});
-
+  final TextStyle style = const TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 10,
+  );
   @override
   Widget build(BuildContext context) {
     if (time.hour > 12) {
       if (time.minute < 10) {
-        return Text("${time.hour - 12}.0${time.minute} P.M");
+        return Text(
+          "${time.hour - 12}.0${time.minute} P.M",
+          style: style,
+        );
       } else {
-        return Text("${time.hour - 12}.${time.minute} P.M");
+        return Text(
+          "${time.hour - 12}.${time.minute} P.M",
+          style: style,
+        );
       }
     } else {
       if (time.minute < 10) {
-        return Text("${time.hour}.0${time.minute} A.M");
+        return Text(
+          "${time.hour}.0${time.minute} A.M",
+          style: style,
+        );
       } else {
-        return Text("${time.hour}.${time.minute} A.M");
+        return Text(
+          "${time.hour}.${time.minute} A.M",
+          style: style,
+        );
       }
     }
   }
