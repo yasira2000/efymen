58ec890f522d113af154a9f87454982940a19c9c
thamindumk <thamindu.20@cse.mrt.ac.lk>
2023-08-31 00:20:04 +0530

    daily routing done and dusted

--- lib/models/timeTable.dart
+++ lib/models/timeTable.dart
@@ -3,14 +3,16 @@ class TimeTable {
   DateTime _startingTime;
   DateTime _endingTime;
   String _location;
+  bool _status;
 
-  TimeTable(
-      this._description, this._startingTime, this._endingTime, this._location);
+  TimeTable(this._description, this._startingTime, this._endingTime,
+      this._location, this._status);
 
   String get description => _description;
   DateTime get startingTime => _startingTime;
   DateTime get endingTime => _endingTime;
   String get location => _location;
+  bool get status => _status;
 
   set description(String description) {
     _description = description;
